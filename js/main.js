a5d12b62dad3e3a0f1407944f6a64b9a471b7c24
YouthClimb <1019865095@qq.com>
2023-01-02 21:39:02 +0800

    进行了部分模块化
    使用 jquery 对网站 Html 文件中复用的 head 部分内容，顶部导航条以及网页底部进行了模块化。现在创建了关于主页和文章页面的 js 文件
    ，准备进一步进行模块化，再修改一些动画内容，将其从 css 转移到 jquery 中。

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    // 加载网页 head 部分内容
+    $("#head").load("../templates/head.html");
+    // 加载网页顶部导航条
+    $("#topbar-warpper").load("../templates/topbar.html");
+    // 加载网页底部
+    $("footer").load("../templates/footer.html");
+}
+);
