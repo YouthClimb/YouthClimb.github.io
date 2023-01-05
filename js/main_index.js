a4abd0ead6f11e8b6c7f3bfad81a26919c424b36
YouthClimb <1019865095@qq.com>
2023-01-05 18:09:40 +0800

    新增动态页面
    新增了主页上文章的动态页面

--- js/main_index.js
+++ js/main_index.js
@@ -3,14 +3,16 @@
 
 $(function () {
     // 点击左侧侧边栏的反馈
-    $('#sidebar a').click(function (event) {
+    $("#sidebar a").click(function (event) {
         event.preventDefault();
         // 加载页面
         let url = $(this).attr('href');
+        
         $('#display-part').load("/templates/display/" + url);
+
     });
 
     // 加载默认页面
-    $('#sidebar li a:nth-child(1)').click();
+    $('#sidebar li a:nth(0)').click();
 }
 );
